abaf40f04f4ea760cac61699e4d8f5be96d0fbdb
marinsara-03 <marin.sara1007@gmail.com>
2025-10-29 19:31:02 -0500

    CodigoArreglo

--- src/parcialprogramacionii/ParcialProgramacionII.java
+++ src/parcialprogramacionii/ParcialProgramacionII.java
@@ -3,18 +3,49 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package parcialprogramacionii;
-
+import java.util.Scanner;
 /**
  *
  * @author flaka
  */
 public class ParcialProgramacionII {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static String[] invertirArray(String[] arrayOriginal) {
+        String[] arrayInvertido = new String[arrayOriginal.length];
+        int j = 0;
+        for (int i = arrayOriginal.length - 1; i >= 0; i--) {
+            arrayInvertido[j] = arrayOriginal[i];
+            j++;
+        }
+        return arrayInvertido;
+    }
+
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner read = new Scanner(System.in);
+        
+        int tamañoArray = 0;
+        
+        System.out.println("Ingrese el tamaño del array: ");
+        tamañoArray = read.nextInt();
+        read.nextLine();
+        
+        String[] arrayIn = new String [tamañoArray];
+        
+        for(int i = 0; i < tamañoArray; i++ ){
+            System.out.println("Ingrese un numero: " + (i));
+            arrayIn[i] = read.nextLine();
+        }System.out.println("Arreglo original: ");
+        for (String s : arrayIn) {
+            System.out.println(s);
+        }
+        
+        String[] invertido = invertirArray(arrayIn);
+        
+        System.out.println("Arreglo invertido:");
+        for (String s : invertido) {
+            System.out.println(s);
+        }
+
     }
     
 }
